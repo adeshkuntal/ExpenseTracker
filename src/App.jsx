e4c2f80ead186942f7a878be1aae1eac8734100b
Adesh Kuntal <adeshkuntal092@gmail.com>
2025-09-06 14:40:43 +0530

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/auth/Login";
+import Login from "./components/auth/login";
 import Dashboard from "./components/dashboard/Dashboard";
 
 const App = () => (
